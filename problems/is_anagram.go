c4a7ff39771ec8857476dafc4c47989910dd75ae
Victor Maklakov <maklakowiktor@gmail.com>
2023-12-17 17:06:17 +0300

    Fixed is_anagram

--- problems/is_anagram.go
+++ problems/is_anagram.go
@@ -8,11 +8,11 @@ func IsAnagram(a string, b string) bool {
 	count := make(map[string]uint, int(math.Max(float64(len(a)), float64(len(b)))))
 
 	for c := range a {
-		count[string(c)] += 1
+		count[string(rune(c))] += 1
 	}
 
 	for c := range b {
-		count[string(c)] -= 1
+		count[string(rune(c))] -= 1
 	}
 
 	for _, v := range count {
